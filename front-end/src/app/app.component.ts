5d3cef9adcd79d2cb574b1b832868fa7e16c98d1
FilipPietryga <filippatrykpietryga@gmail.com>
2025-03-11 21:18:25 +0100

    Created a task model

--- front-end/src/app/app.component.ts
+++ front-end/src/app/app.component.ts
@@ -1,10 +1,11 @@
+import { HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { TaskListComponent } from "./task-list/task-list.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [HttpClientModule, TaskListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.sass'
 })

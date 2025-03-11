efe6ac2165853cbee0f29cd35a7a87f51e321fd4
FilipPietryga <filippatrykpietryga@gmail.com>
2025-03-11 21:20:10 +0100

    Updated the task service

--- front-end/src/app/task-list/task-list.component.ts
+++ front-end/src/app/task-list/task-list.component.ts
@@ -1,22 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { TaskService } from '../task.service';
+import { TaskComponent } from "../task/task.component";
+import { Task } from '../task';
 
 @Component({
   selector: 'app-task-list',
   standalone: true,
-  imports: [],
+  imports: [TaskComponent],
   templateUrl: './task-list.component.html',
   styleUrl: './task-list.component.sass'
 })
-export class TaskListComponent {
-  tasks: string[] = [];
+export class TaskListComponent implements OnInit {
+  tasks: Task[] = [];  
 
   constructor(private taskService: TaskService) {}
 
   ngOnInit(): void {
     this.taskService.getTasks().subscribe(
       (data) => {
-        this.tasks = data;
+        this.tasks = data;  
       },
       (error) => {
         console.error('Error fetching tasks:', error);
--- front-end/src/app/task.service.ts
+++ front-end/src/app/task.service.ts
@@ -1,17 +1,17 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { Task } from './task';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TaskService {
+  private apiUrl = 'http://localhost:8080/api/tasks';  
 
-  private apiUrl = 'http://localhost:8080/api/tasks';
+  constructor(private http: HttpClient) {}
 
-  constructor(private http: HttpClient) { }
-
-  getTasks(): Observable<string[]> {
-    return this.http.get<string[]>(this.apiUrl);
+  getTasks(): Observable<Task[]> {
+    return this.http.get<Task[]>(this.apiUrl);
   }
 }

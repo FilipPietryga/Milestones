28e48d8ced65b338a464882a195e1154b59a5375
FilipPietryga <filippatrykpietryga@gmail.com>
2025-03-11 21:01:51 +0100

    Initialized back-end

---
+++ back-end/src/main/java/com/milestones/MilestonesApplication.java
@@ -0,0 +1,13 @@
+package com.milestones;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MilestonesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MilestonesApplication.class, args);
+	}
+
+}
---
+++ back-end/src/test/java/com/milestones/MilestonesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.milestones;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MilestonesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
